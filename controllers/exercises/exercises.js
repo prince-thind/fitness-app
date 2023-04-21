5800d8e1648422df24e68cfde35eb3cbcaa5c3a4
Prince Thind <princethind799@gmail.com>
2023-04-21 17:22:05 +0530

    implement dummy exercise controllers

---
+++ controllers/exercises/exercises.js
@@ -0,0 +1,11 @@
+exports.index = async function (req, res) {
+  res.json({ data: { exercises: ["TODO"] } });
+};
+
+exports.create = async function (req, res) {
+  res.json({ data: { exercise: "TODO" } });
+};
+
+exports.delete = async function (req, res) {
+  res.json({ data: { success: "TODO" } });
+};
---
+++ routes/exercises.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const exercisesController = require("../controllers/exercises/exercises");
+
+router.get("/", exercisesController.index);
+router.post("/create", exercisesController.create);
+router.delete("/delete", exercisesController.delete);
+
+module.exports = router;
