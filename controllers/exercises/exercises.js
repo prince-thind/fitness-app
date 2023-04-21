6d6033d4fba0bf6477175eb0b754f98911112c36
Prince Thind <princethind799@gmail.com>
2023-04-21 18:49:12 +0530

    implement basic validation for exercise api

--- controllers/exercises/exercises.js
+++ controllers/exercises/exercises.js
@@ -7,6 +7,14 @@ exports.index = async function (req, res) {
 
 exports.create = async function (req, res) {
   const { exerciseName, exerciseLength } = req.body;
+
+  const alreadyPresentExercise = await exercise.findOne({ exerciseName });
+  if (alreadyPresentExercise) {
+    return res.json({
+      error: { code: 403, message: "Exercise Name already exists" },
+    });
+  }
+
   const exercise = new Exercise({
     exerciseName,
     exerciseLength,
@@ -19,6 +27,12 @@ exports.create = async function (req, res) {
 exports.delete = async function (req, res) {
   const { id } = req.params;
   const exercise = await Exercise.findByIdAndDelete(id);
+
+  if (!exercise) {
+    return res.json({
+      error: { code: 404, message: "Exercise does not exist" },
+    });
+  }
 
   return res.json({ data: { exercise } });
 };
