50402ed1b657a0165c39ed143bc7bc4c0944b3b0
Prince Thind <princethind799@gmail.com>
2023-04-22 12:17:33 +0530

    create basic navigational header

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function Layout() {
   return (
     <div>
-      layout stuff
-      <Outlet />
+      <header className="header">
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/programs">Programs</Link>
+          </li>{" "}
+          <li>
+            <Link to="/exercises">Exercises</Link>
+          </li>{" "}
+          <li>
+            <Link to="/programs/create">Create Program</Link>
+          </li>{" "}
+          <li>
+            <Link to="/exercises/create">Create Exercise</Link>
+          </li>
+        </ul>
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
