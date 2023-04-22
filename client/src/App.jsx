90dd5dee81bf81b06332c5421b89f5b86eeaaa3a
Prince Thind <princethind799@gmail.com>
2023-04-22 12:02:20 +0530

    initial react router dummy routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,57 @@
-function App() {
-  return (
-    <>
-      <div>hello world</div>
-    </>
-  );
-}
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
-export default App;
+import Home from "./pages/Home.jsx";
+import Layout from "./pages/Layout.jsx";
+import Programs from "./pages/Programs.jsx";
+import Program from "./pages/Program.jsx";
+import CreateProgram from "./pages/CreateProgram.jsx";
+import UpdateProgram from "./pages/UpdateProgram.jsx";
+import Exercises from "./pages/Exercises.jsx";
+import Exercise from "./pages/Exercise.jsx";
+import CreateExercise from "./pages/CreateExercise.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/programs",
+        element: <Programs />,
+      },
+      {
+        path: "/programs/:id",
+        element: <Program />,
+      },
+      {
+        path: "/programs/create/",
+        element: <CreateProgram />,
+      },
+      {
+        path: "/programs/update/:id",
+        element: <UpdateProgram />,
+      },
+      {
+        path: "/exercises",
+        element: <Exercises />,
+      },
+      {
+        path: "/exercises/:id",
+        element: <Exercise />,
+      },
+      {
+        path: "/exercises/create/",
+        element: <CreateExercise />,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
